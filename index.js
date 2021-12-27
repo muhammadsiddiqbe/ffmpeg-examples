186f76714519f2dee7fddead571b88c1e4bc9523
Muhammadsiddiq <62147139+muhammadsiddiqbe@users.noreply.github.com>
2021-12-27 10:25:21 +0500

    make some changes

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const ffmpeg = require("fluent-ffmpeg");
 const path = require("path");
-const fs = require("fs");
 
-const videoPath = path.join(__dirname, "./videos/video.mp4");
+const video = path.join(__dirname, "./videos/video.mp4");
 
 // // mute audio of video
-ffmpeg(videoPath)
+ffmpeg(video)
   .noAudio()
   .output(path.join(__dirname, "./videos/video-no-audio.mp4"))
   .on("end", () => {
@@ -14,7 +13,7 @@ ffmpeg(videoPath)
   .run();
 
 // // change resolution of video
-ffmpeg(videoPath)
+ffmpeg(video)
   .size("320x240")
   .output(path.join(__dirname, "./videos/video-320x240.mp4"))
   .on("end", () => {
@@ -23,7 +22,7 @@ ffmpeg(videoPath)
   .run();
 
 // // change quality of video
-ffmpeg(videoPath)
+ffmpeg(video)
   .outputOptions(["-qscale:v", "8"])
   .output(path.join(__dirname, "./videos/video-qscale-2.mp4"))
   .on("end", () => {
@@ -32,7 +31,7 @@ ffmpeg(videoPath)
   .run();
 
 //change fps of video and compress it
-ffmpeg(videoPath)
+ffmpeg(video)
   .outputOptions(["-r", "15/1"])
   .output(path.join(__dirname, "./videos/video-fps-15.mp4"))
   .on("end", () => {
@@ -41,7 +40,6 @@ ffmpeg(videoPath)
   .run();
 
 // show video info
-ffmpeg.ffprobe(videoPath, (err, data) => {
+ffmpeg.ffprobe(video, (err, data) => {
   console.log(data.streams[0]);
 });
-
